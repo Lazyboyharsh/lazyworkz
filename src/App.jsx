ff45e80f2df762fef88339b79035ad40aaf8e841
lazyboyharsh <hharshjain448@gmail.com>
2026-01-03 21:53:47 +0530

    fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
+
 
 // Pages
 import Home from './pages/Home';
